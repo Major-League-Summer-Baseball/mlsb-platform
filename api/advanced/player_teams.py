738d4b4cab5f57ca8425a2de89bf49dedd354dd8
fras2560 <fras2560@mylaurier.ca>
2016-04-20 16:03:55 -0400

    Fuck me

---
+++ api/advanced/player_teams.py
@@ -0,0 +1,41 @@
+'''
+@author: Dallas Fraser
+@author: 2016-04-12
+@organization: MLSB API
+@summary: The views for the teams a player is on
+'''
+from flask.ext.restful import Resource, reqparse
+from flask import Response
+from json import dumps
+from api.model import Player
+parser = reqparse.RequestParser()
+parser.add_argument('email', type=str)
+parser.add_argument('player_name', type=str)
+
+class PlayerTeamLookupAPI(Resource):
+    def post(self):
+        """
+            POST request to lookup a Player's teams
+            Route: Route['player_lookup']
+            Parameters:
+                email: the league id (str)
+                player_name: the player id (str)
+            Returns:
+                status: 200 
+                mimetype: application/json
+                data: list of possible Teams
+        """
+        data = []
+        args = parser.parse_args()
+        players = None
+        if args['email']:
+            # guaranteed to find player
+            players = Player.query.filter(Player.email==args['email']).all()
+        elif args['player_name']:
+            # maybe overlap
+            players = Player.query.filter(Player.name.contains(args['player_name'])).all()
+        if players is not None:
+            for player in players:
+                for team in player.teams:
+                    data.append(team.json())
+        return Response(dumps(data), status=200, mimetype="application/json")
