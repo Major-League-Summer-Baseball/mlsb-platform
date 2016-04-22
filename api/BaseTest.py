08b55fc034e4cc7826d72e79b96debcf9bfcbdf0
fras2560 <fras2560@mylaurier.ca>
2016-04-21 22:25:49 -0400

    weird import issue

--- api/BaseTest.py
+++ api/BaseTest.py
@@ -10,7 +10,16 @@ from api import DB
 from pprint import PrettyPrinter
 from api.model import Player, Team, Sponsor, League, Game, Bat, Espys, Fun
 from datetime import datetime, timedelta
-from api.credentials import ADMIN, PASSWORD, KIK, KIKPW
+import os
+try:
+    # running local
+    local = True
+    from api.credentials import ADMIN, PASSWORD, KIK, KIKPW
+except:
+    ADMIN = os.environ['ADMIN']
+    PASSWORD = os.environ['PASSWORD']
+    KIK = os.environ['KIK']
+    KIKPW = os.environ['KIKPW']
 from base64 import b64encode
 headers = {
     'Authorization': 'Basic %s' % b64encode(bytes(ADMIN + ':' + PASSWORD, "utf-8")).decode("ascii")
