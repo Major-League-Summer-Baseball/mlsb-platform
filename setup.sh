18f933dba05d2e367ec05bd378c1bd2970d34b95
fras2560 <dallas.fraser.waterloo@gmail.com>
2018-08-13 17:44:56 -0400

    Added a brackets
    	Should make it more clear

--- setup.sh
+++ setup.sh
@@ -4,44 +4,44 @@ echo "========================================="
 
 if [[ -z "${ADMIN}" ]]; then
   ADMIN="admin"
-  echo "ADMIN environment variable not set default=admin"
+  echo "ADMIN environment variable not set (default=admin)"
   export ADMIN
 fi
 
 if [[ -z "${PASSWORD}" ]]; then
   PASSWORD="password"
-  echo "PASSWORD environment variable not set default=password"
+  echo "PASSWORD environment variable not set (default=password)"
   export PASSWORD
 fi
 
 
 if [[ -z "${DATABASE_URL}" ]]; then
   DATABASE_URL="postgresql://$ADMIN:$PASSWORD@postgres:5432/mlsb"
-  echo "DATABASE_URL environment variable not set default=postgresql://$ADMIN:$PASSWORD@postgres:5432/mlsb"
+  echo "DATABASE_URL environment variable not set (default=postgresql://$ADMIN:$PASSWORD@postgres:5432/mlsb)"
   export DATABASE_URL
 fi
   
 if [[ -z "${KIK}" ]]; then
   KIK="kik"
-  echo "KIK environment variable not set default=kik"
+  echo "KIK environment variable not set (default=kik)"
   export KIK
 fi
 
 if [[ -z "${KIKPW}" ]]; then
   KIKPW="password"
-  echo "KIKPW environment variable not set default=password"
+  echo "KIKPW environment variable not set (default=password)"
   export KIKPW
 fi
 
 if [[ -z "${SECRET_KEY}" ]]; then
   SECRET_KEY="secret"
-  echo "SECRET_KEY environment variable not set default=secret"
+  echo "SECRET_KEY environment variable not set (default=secret)"
   export SECRET_KEY
 fi
 
 if [[ -z "${REDIS_URL}" ]]; then
   REDIS_URL="secret"
-  echo "REDIS_URL environment variable not set default=redis://redis:6379/1"
+  echo "REDIS_URL environment variable not set (default=redis://redis:6379/1)"
   export REDIS_URL
 fi
 
