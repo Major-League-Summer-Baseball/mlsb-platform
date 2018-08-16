0b8fadf83ec1e94a8cbb0c7cd0101706f3883dfb
fras2560 <dallas.fraser.waterloo@gmail.com>
2018-08-16 17:43:04 -0400

    Added a sleep to ensure database is up and running before calling init

--- setup.sh
+++ setup.sh
@@ -50,6 +50,9 @@ docker-compose build
 echo "Done building"
 docker-compose up -d
 echo "App is up and runnning"
+
+# sleep to ensure the database is up and running before calling init
+sleep 5s
 winpty docker-compose exec mlsb python initDB.py
 echo "Database tables created"
 
