504632df7a812b0b098c526c566be308278659fa
fras2560 <dallas.fraser.waterloo@gmail.com>
2019-01-04 17:56:28 -0500

    Getting it to run in headless mode

--- features/environment.py
+++ features/environment.py
@@ -9,6 +9,7 @@ from behave import fixture, use_fixture
 from selenium import webdriver
 from api import app
 import os
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 BASE_URL = "http://localhost:5000"
 DELAY = 5
 
@@ -24,7 +25,12 @@ def after_step(context, step):
 def selenium_browser_chrome(context):
 
     # start the browser
-    context.browser = webdriver.Chrome()
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--window-size=1920,1080")
+    context.browser = webdriver.Chrome(chrome_options=chrome_options)
     yield context.browser
 
     # clean up
