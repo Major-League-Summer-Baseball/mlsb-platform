352c34bf7198f605db685125455942f3f9d2caa6
fras2560 <dallas.fraser.waterloo@gmail.com>
2019-03-13 21:17:52 -0400

    Update so does not run test suites

--- run_tests.sh
+++ run_tests.sh
@@ -7,10 +7,10 @@ read -p "Are you sure you want to continue [Y/y]: "  cont
 
 if [ $cont = "Y" ]; then
     echo "Running tests"
-    winpty docker-compose exec mlsb python -m unittest discover -s api/test
+    winpty docker-compose exec mlsb python -m unittest discover -s api/test -p test*.py
 elif [  $cont = "y" ]; then
     echo "Running tests"
-    winpty docker-compose exec mlsb python -m unittest discover -s api/test
+    winpty docker-compose exec mlsb python -m unittest discover -s api/test -p test*.py
 else
     echo "Not running tests, have a nice day"
 fi
