2477c449cf7092dcc8c89c8f743efe6ee1b06642
fras2560 <dallas.fraser.waterloo@gmail.com>
2019-04-08 23:04:16 -0400

    Removed un-used constants and deprecated methods

--- api/advanced/import_team.py
+++ api/advanced/import_team.py
@@ -13,18 +13,12 @@ from api.errors import InvalidField, SponsorDoesNotExist, LeagueDoesNotExist
 import logging
 
 # constants
-CREATED = "Created Team (no league was specified)"
-NO_SPONSOR = "No sponsor, please ensure spelt correctly or create sponsor"
-INVALID_FILE = "File was not given in right format (use template)"
-EXAMPLE_FOUND = "First entry contain the templates example, just skipped it"
-EMAIL_NAME = "Player {} email was found but had different name"
-INVALID_FIELD = "{} had an invalid field"
 MISSING_BACKGROUND = "Missing background: {}"
 LEFT_BACKGROUND_EXAMPLE = "Background example was left: {}"
 LEFT_PLAYER_EXAMPLE = "Player example was left:  {}"
 INVALID_SPONSOR = "Sponsor given was not found: {}"
-INVALID_LEAGUE = "League given was not found: {}"
 INVALID_PLAYER = "Player given {} had the following issue: {}"
+INVALID_LEAGUE = "League given was not found: {}"
 PLAYER_MISMATCH_COLUMNS = "Player mismatched the headers: {}"
 INVALID_ROW = "Unsure what to do with the following row: {}"
 PLAYER_ROW_IDENTIFIER = "player"
@@ -131,158 +125,6 @@ class TeamList():
             self.warnings.append(CAPTAIN_NOT_ASSIGNED)
         self.session.commit()
 
-    def add_team(self):
-        """Adds a team to the database"""
-        player_index = self.import_headers()
-        self.logger.debug(self.lines[player_index - 1])
-        self.set_columns_indices(self.lines[player_index - 1].split(","))
-        self.logger.debug(self.name_index, self.email_index, self.gender_index)
-        self.import_players(player_index)
-        self.logger.debug("Imported players")
-        # should be good no errors were raised
-        self.session.commit()
-        if self.captain is None:
-            self.warnings.append("Captain was not assigned")
-        else:
-            # set the captain
-            self.team.player_id = self.captain.id
-            self.session.commit()
-        self.logger.debug("Captain set")
-        return
-
-    def import_headers(self):
-        """Parse the headers of the csv and add team"""
-        done = False
-        i = 0
-        sponsor = None
-        color = None
-        captain = None
-        league = None
-        while not done:
-            # read the headers
-            line = self.lines[i]
-            columns = line.split(",")
-            if self.clean_cell(columns[0]) == "sponsor":
-                sponsor = columns[1].strip()
-            elif self.clean_cell(columns[0]) == "color":
-                color = columns[1].strip()
-            elif self.clean_cell(columns[0]) == "captain":
-                captain = columns[1].strip()
-            elif self.clean_cell(columns[0]) == "league":
-                league = columns[1].strip()
-            else:
-                # assuming done reading the headers
-                done = True
-            i += 1
-        player_index = i
-        if sponsor is None:
-            raise InvalidField(payload={"details": "No sponsor was given"})
-        if captain is None:
-            raise InvalidField(payload={"details": "No captain was given"})
-        if color is None:
-            raise InvalidField(payload={"details": "No color was given"})
-        if league is None:
-            raise InvalidField(payload={"details": "No league was given"})
-        # check no examples were left and actually real info
-        if (league.lower().startswith("ex.") or
-            sponsor.lower().startswith("ex.") or
-            color.lower().startswith("ex.") or
-            captain.lower().startswith("ex.")):
-            t = "The header's still had an example"
-            raise InvalidField(payload={"details": t})
-        sponsor_id = (DB.session.query(Sponsor).filter(Sponsor.name == sponsor)
-                      .first())
-        if sponsor_id is None:
-            sponsor_id = (DB.session.query(Sponsor)
-                          .filter(Sponsor.nickname == sponsor)
-                          .first())
-        if sponsor_id is None:
-            # what kind of sponsor are you giving
-            t = "The sponsor does not exist (check case)"
-            raise SponsorDoesNotExist(payload={'details': t})
-        sponsor_id = sponsor_id.id
-        league_id = (DB.session.query(League)
-                     .filter(League.name == league)).first()
-        if league_id is None:
-            t = "The league does not exist (check case)"
-            raise LeagueDoesNotExist(payload={"details": t})
-        league_id = league_id.id
-        # check to see if team was already created
-        teams = (DB.session.query(Team)
-                 .filter(Team.color == color)
-                 .filter(Team.sponsor_id == sponsor_id)
-                 .filter(Team.year == date.today().year).all())
-        team_found = None
-        if len(teams) > 0:
-            team_found = teams[0]
-        # was the team not found then should create it
-        if team_found is None:
-            self.warnings.append("Team was created")
-            team_found = Team(color=color,
-                              sponsor_id=sponsor_id,
-                              league_id=league_id)
-        else:
-            team_found.players = []  # remove all the players before
-        self.team = team_found  # set the team
-        self.captain_name = captain  # remember captains name
-        return player_index
-
-    def import_players(self, player_index):
-        """Import a list of players"""
-        while (player_index < len(self.lines) and
-               len(self.lines[player_index].split(",")) > 1):
-            info = self.lines[player_index].split(",")
-            self.logger.debug(info, len(info))
-            if len(info) < 3:
-                raise InvalidField(payload={"details": "Missing a category"})
-            name = info[self.name_index].strip()
-            email = info[self.email_index].strip()
-            gender = info[self.gender_index].strip()
-            if not name.lower().startswith("ex."):
-                player = (DB.session.query(Player)
-                            .filter(Player.email == email)).first()
-                if player is None:
-                    self.logger.debug(name + " " + email + " " + gender)
-                    player = Player(name, email, gender=gender)
-                    self.session.add(player)
-                    self.logger.debug("Player was created")
-                else:
-                    # this player is active
-                    player.active = True
-                self.team.players.append(player)
-                if name == self.captain_name:
-                    self.captain = player
-            else:
-                self.warnings.append("Player Example was left")
-            player_index += 1
-        return
-
-    def clean_cell(self, cell):
-        """Returns a clean cell"""
-        return cell.strip().lower().replace(":", "")
-
-    def set_columns_indices(self, header):
-        '''
-        a method that set the indices for the column headers
-        Parameters:
-            header: the list of columns for the header (list of str)
-        Returns:
-        '''
-        for i in range(0, len(header)):
-            if "email" in header[i].lower():
-                self.email_index = i
-            elif "name" in header[i].lower():
-                self.name_index = i
-            elif "gender" in header[i].lower():
-                self.gender_index = i
-        if self.email_index is None:
-            raise InvalidField(payload={'details': "Email header missing"})
-        if self.name_index is None:
-            raise InvalidField(payload={'details': "Player header missing"})
-        if self.gender_index is None:
-            raise InvalidField(payload={'details': "Gender header missing"})
-        return
-
 
 def extract_background(background):
     """Returns a dictionary of the extracted json objects from the background.
@@ -359,19 +201,22 @@ def extract_column_indices_lookup(header):
     """
     lookup = {}
     for i in range(0, len(header)):
-        if "email" in header[i].lower():
-            lookup["email"] = i
-        elif "name" in header[i].lower():
-            lookup["name"] = i
-        elif "gender" in header[i].lower():
-            lookup["gender"] = i
-    if "email" not in lookup:
-        raise InvalidField(payload={'details': "Email header missing"})
-    if "name" not in lookup:
-        raise InvalidField(payload={'details': "Player header missing"})
-    if "gender" not in lookup:
-        raise InvalidField(payload={'details': "Gender header missing"})
+        for key, value in HEADERS.items():
+            if is_entry_a_header(key, value, header[i]):
+                lookup[key.lower()] = i
+
+    # ensure all headers were found
+    for key in HEADERS.keys():
+        if key not in lookup.keys():
+            error_message = "{} header missing".format(key.lower())
+            raise InvalidField(payload={'details': error_message})
     return lookup
+
+
+def is_entry_a_header(key, value, entry):
+    """Returns whether the given entry in the header is a expected header."""
+    return (key.lower() in entry.lower()
+            or value.lower() in entry.lower())
 
 
 def extract_player_information(info, lookup):
