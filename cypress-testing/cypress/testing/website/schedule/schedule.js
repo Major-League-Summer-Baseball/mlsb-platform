e1c6d411e63cb0638cb41a24e44f776891e2a9c2
Dallas Fraser <dfraser@digitaled.com>
2019-11-28 20:31:58 -0500

    Simple test for class schedule
    Just writing a simple test for class schedule for now

---
+++ cypress-testing/cypress/testing/website/schedule/schedule.js
@@ -0,0 +1,37 @@
+/* eslint new-cap: [2, {capIsNewExceptions: ["Given", "When", "Then"]}]*/
+
+/**
+ * Holds steps related to the schedule feature.
+ * @module website/schedule
+ */
+
+import {Given, When, Then} from 'cypress-cucumber-preprocessor/steps';
+import {getCurrentYear} from '../../common/helper.js';
+
+/**
+ * The URL for the league schedule.
+ * @private
+ */
+const SCHEDULE_PAGE = 'website/schedule/';
+
+
+/**
+ * A step to navigate to the player stats page.
+ * @example
+ * Given I am on the the schedule page
+ */
+const navigateToSchedulePage = () => {
+  cy.visit({url: SCHEDULE_PAGE + getCurrentYear(), method: 'GET'});
+};
+Given(`I am on the the schedule page`, navigateToSchedulePage);
+
+/**
+ * A step to assert the schedule page properly displayed
+ * @example
+ * Then I see the schedule for the league
+ */
+const assertScheduleDisplayed = () => {
+  cy.get('td').contains('WP1');
+  cy.get('th').contains('Score');
+};
+Then(`I see the schedule for the league`, assertScheduleDisplayed);
