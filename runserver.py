4cf7e6fe51ae2a7518dcf1694f268e8293440aa6
Dallas Fraser <dfraser@digitaled.com>
2020-03-19 16:43:23 -0400

    When starting the server try a handful of port for the app to run on.

--- runserver.py
+++ runserver.py
@@ -6,4 +6,13 @@ Purpose: To create an application to act as an api for the database
 '''
 from api import app
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+    start = False
+    port = 5000
+    while not start and port < 5010:
+        try:
+            app.run(host='0.0.0.0', debug=True, port=port)
+            start = True
+        except OSError as e:
+            print(e)
+            print(f"Port:{port} taken trying another")
+            port += 1
