0f810bb0b4ba94409963b917e1ef3b2f85f7a3bf
fras2560 <dallas.fraser.waterloo@gmail.com>
2021-04-17 13:50:46 -0400

    Add some simple tests for login views\
    The one were logged in might be more difficult

--- api/test/testViews.py
+++ api/test/testViews.py
@@ -5,8 +5,11 @@
 @summary: Tests some views at least do not respond with application error
 '''
 from api.routes import Routes
+from flask import url_for
+from flask_login import login_user
 from api.test.BaseTest import TestSetup, SUCCESSFUL_GET_CODE, INVALID_ID,\
     REDIRECT_CODE
+from api.authentication import load_user
 from datetime import datetime
 import uuid
 START_OF_PLATFORM = 2016
@@ -21,6 +24,12 @@ class TestWebsiteViews(TestSetup):
                               "Logo page should give a 200")
         self.assertGetRequest(Routes['privacy'],
                               "Privacy page should give a 200")
+
+    def testLoginPages(self):
+        self.assertGetRequest("/authenticate", "Need to login")
+        self.assertGetRequest("/login", "Login page")
+        self.assertGetRequest("/request_sent",
+                            "League request sent page")
 
     def testSponsorsPages(self):
         sponsor_name = str(uuid.uuid1())
