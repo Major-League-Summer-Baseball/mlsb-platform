89859d21a0dd87c3d089e373daa55b2d278ac5d1
Dallas Fraser <dallas.fraser@knowledgehook.com>
2022-03-13 16:17:49 -0400

    Add the new events and clean up some old ones
    Beerlympics instead of beerfest
    Grandbender instead of beerwell
    
    Remove hitting for the cycle since it has been years
    
    Add this year events.

--- api/variables.py
+++ api/variables.py
@@ -30,26 +30,26 @@ GENDERS = ["f", "m"]
 # all the events baby
 EVENTS = {
     2016: {
-        "Beerfest": "May 13th",
+        "Beerlympics": "May 13th",
         "ESPYS_Awards": "July 27th",
         "Jays_Game": "May 17th",
         "Summerween": "June 11th",
         "Mystery_Bus": "March 19th",
         "Rafting": "July 9th",
         "Hitting_for_the_Cycle": "July 16th",
-        "Beerwell_Classic": "July 23rd",
+        "Grand_Bender": "July 23rd",
         "Tournaments": "May 21st, May27th, July 15th, July 22nd",
         "MLSB_Alumni": "August 13th"
     },
     2017: {
-        "Beerfest": "May 17th",
+        "Beerlympics": "May 17th",
         "ESPYS_Awards": "July 28th",
         "Jays_Game": "May 9th",
         "Summerween": "June 3rd",
         "Mystery_Bus": "March 24th",
         "Rafting": "July 7-9th",
         "Hitting_for_the_Cycle": "July 16th",
-        "Beerwell_Classic": "?",
+        "Grand_Bender": "?",
         "Tournaments": "May 19th, June 9th, July 14th, July 21st",
         "MLSB_Alumni": "August 12th"
     },
@@ -59,7 +59,7 @@ EVENTS = {
         "Summerween": "June 23rd",
         "Mystery_Bus": "March 24th",
         "Rafting": "July 6-8th",
-        "Grand Bender": "July 28th",
+        "Grand_Bender": "July 28th",
         "Tournaments": "June 1st, June 8th, June 29th, July 20th",
         "Chainsaw Idol": "July 13th",
         "MLSB_Alumni": "July 21st"
@@ -70,7 +70,7 @@ EVENTS = {
         "Summerween": "June 21st",
         "Mystery_Bus": "March 22nd",
         "Rafting": "July 5th-7th",
-        "Grand Bender": "July 29th",
+        "Grand_Bender": "July 29th",
         "Tournaments": "June 28th, July 12th, July 19th",
         "Chainsaw Idol": "???",
         "MLSB_Alumni": "July 20th"
@@ -81,7 +81,7 @@ EVENTS = {
         "Summerween": "Cancelled",
         "Mystery_Bus": "March 7th",
         "Rafting": "Cancelled",
-        "Grand Bender": "Cancelled",
+        "Grand_Bender": "Cancelled",
         "Tournaments": "Cancelled",
         "Chainsaw Idol": "Cancelled",
         "MLSB_Alumni": "July 25th"
@@ -92,11 +92,23 @@ EVENTS = {
         "Summerween": "TBD",
         "Mystery_Bus": "TBD",
         "Rafting": "TBD",
-        "Grand Bender": "TBD",
+        "Grand_Bender": "TBD",
         "Tournaments": "TBD, TBD, TBD, TBD",
         "Chainsaw Idol": "TBD",
         "MLSB_Alumni": "July 25th"
-    }
+    },
+    2022: {
+        "Beerlympics": "June 11th",
+        "Jays_Game": "May 24th",
+        "Summerween": "June 25",
+        "Mystery_Bus": "TBD",
+        "Rafting": "July 8th-10th",
+        "Grand_Bender": "July 23rd",
+        "Tournaments": "June 3rd, June 17th, TBD, TBD",
+        "Chainsaw Idol": "TBD",
+        "MLSB_Alumni": "July 23rd"
+    },
+
 }
 
 # the fields we play at
