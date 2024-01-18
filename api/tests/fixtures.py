c5df6f409fabac3257ed78cbc2acd40f7c44e447
fras2560 <dallas.fraser.waterloo@gmail.com>
2024-01-17 20:07:25 -0500

    Add a factory for creating sponsor

--- api/tests/fixtures.py
+++ api/tests/fixtures.py
@@ -1,5 +1,6 @@
 import pytest
 from api.app import create_app
+from api.model import Sponsor
 
 
 @pytest.fixture(scope="session")
@@ -12,7 +13,6 @@ def mlsb_app():
     })
 
     # other setups
-
     yield mlsb_app
 
     # clean up / reset resources
@@ -27,3 +27,16 @@ def client(mlsb_app):
 @pytest.fixture(scope="session")
 def runner(mlsb_app):
     return mlsb_app.test_cli_runner()
+
+
+def factory_fixture(factory):
+    @pytest.fixture(scope='session')
+    def maker():
+        return factory
+    maker.__name__ = factory.__name__
+    return maker
+
+
+@factory_fixture
+def sponsor_factory(sponsor_name, link=None, description=None):
+    return Sponsor(sponsor_name, link=link, description=description)
