a1e275006f3d1b0c50c5ae937337f62759e6725f
fras2560 <dallas.fraser.waterloo@gmail.com>
2024-01-17 22:58:49 -0500

    Add factories for league_event and league_event_date
    Also make all parameters option with reasonable defaults

--- api/tests/conftest.py
+++ api/tests/conftest.py
@@ -1,3 +1,3 @@
 import pytest
 from api.tests.fixtures import mlsb_app, client, runner, sponsor_factory,\
-    player_factory
+    player_factory, league_event_factory, league_event_date_factory
--- api/tests/fixtures.py
+++ api/tests/fixtures.py
@@ -1,7 +1,8 @@
 import pytest
+import uuid
 from api.app import create_app
 from api.extensions import DB
-from api.model import Sponsor, Player
+from api.model import Sponsor, Player, LeagueEvent, LeagueEventDate
 
 
 @pytest.fixture(scope="session")
@@ -38,7 +39,8 @@ def factory_fixture(factory):
 
 
 @factory_fixture
-def sponsor_factory(sponsor_name, link=None, description=None):
+def sponsor_factory(sponsor_name: str = '', link=None, description=None):
+    sponsor_name = sponsor_name if sponsor_name != '' else f"Sponsor - {str(uuid.uuid4())}"
     sponsor = Sponsor(sponsor_name, link=link, description=description)
     DB.session.add(sponsor)
     DB.session.commit()
@@ -46,7 +48,9 @@ def sponsor_factory(sponsor_name, link=None, description=None):
 
 
 @factory_fixture
-def player_factory(name, email, gender, password="default", active=True):
+def player_factory(name: str = '', email: str = '', gender="M", password="default", active=True):
+    name = name if name != '' else f"{str(uuid.uuid4())}"
+    email = email if email != '' else f"{str(uuid.uuid4())}@mlsb.ca"
     player = Player(
         name=name,
         email=email,
@@ -57,3 +61,39 @@ def player_factory(name, email, gender, password="default", active=True):
     DB.session.add(player)
     DB.session.commit()
     return player
+
+
+@factory_fixture
+def league_event_factory(
+    name: str = '',
+    description: str = 'Description',
+    active: bool = True
+) -> LeagueEvent:
+    name = name if name != '' else f"Event - {str(uuid.uuid4())}"
+    league_event = LeagueEvent(
+        name=name,
+        description=description,
+        active=active,
+    )
+    DB.session.add(league_event)
+    DB.session.commit()
+    return league_event
+
+
+@factory_fixture
+def league_event_date_factory(
+    league_event: LeagueEvent,
+    date: str = "2022-10-01",
+    time: str = "10:00",
+    attendees: list[Player] = []
+) -> LeagueEventDate:
+    league_event_date = LeagueEventDate(
+        date=date,
+        time=time,
+        league_event_id=league_event.id,
+    )
+    for player in attendees:
+        league_event_date.signup_player(player.id)
+    DB.session.add(league_event)
+    DB.session.commit()
+    return league_event_date
