225035cf5089b21afaa34a8ccd479955ca31b4d7
fras2560 <dallas.fraser.waterloo@gmail.com>
2024-01-22 08:44:05 -0500

    Test the standings page
    Will add unit test for the underlying methods like
    player_Summary, get_league_leaders and get_espys_breakdown
    So this tests should just check for 200 since underlying method is tested and actual html page is tested in cypress

---
+++ api/tests/website/test_standings.py
@@ -0,0 +1,58 @@
+import pytest
+from datetime import datetime, date
+from api.model import League
+from flask import url_for
+from json import dumps
+from api.helper import loads
+
+THIS_YEAR = date.today().year
+
+@pytest.mark.usefixtures('client')
+@pytest.mark.usefixtures('mlsb_app')
+def test_espys_breakdown_request(mlsb_app, client):
+    with mlsb_app.app_context():
+        url = url_for("website.espys_breakdown_request", year=THIS_YEAR)
+        response = client.get(url, follow_redirects=True)
+        assert response.status_code == 200
+        response_data = loads(response.data)
+        assert response_data['name'] == "ESPYS Breakdown"
+        assert isinstance(response_data['children'], list) is True
+
+
+@pytest.mark.usefixtures('client')
+@pytest.mark.usefixtures('mlsb_app')
+def test_all_time_leaders_page(mlsb_app, client):
+    with mlsb_app.app_context():
+        url = url_for("website.all_time_leaders_page", year=THIS_YEAR)
+        response = client.get(url, follow_redirects=True)
+        assert response.status_code == 200
+
+
+@pytest.mark.usefixtures('client')
+@pytest.mark.usefixtures('mlsb_app')
+def test_leaders_page(mlsb_app, client):
+    with mlsb_app.app_context():
+        url = url_for("website.leaders_page", year=THIS_YEAR)
+        response = client.get(url, follow_redirects=True)
+        assert response.status_code == 200
+
+
+@pytest.mark.usefixtures('client')
+@pytest.mark.usefixtures('mlsb_app')
+def test_stats_page(mlsb_app, client):
+    with mlsb_app.app_context():
+        url = url_for("website.stats_page", year=THIS_YEAR)
+        response = client.get(url, follow_redirects=True)
+        assert response.status_code == 200
+
+
+@pytest.mark.usefixtures('client')
+@pytest.mark.usefixtures('mlsb_app')
+def test_standings_page(mlsb_app, client):
+    with mlsb_app.app_context():
+        league = League.query.first()
+        url = url_for(
+            "website.standings", league_id = league.id, year=THIS_YEAR
+        )
+        response = client.get(url, follow_redirects=True)
+        assert response.status_code == 200
