05129cf0589897024b18ac9d7c494423890121ac
fras2560 <dallas.fraser.waterloo@gmail.com>
2024-01-28 20:11:28 -0500

    Remove the sample league from conftest

--- api/tests/conftest.py
+++ api/tests/conftest.py
@@ -2,4 +2,4 @@ import pytest
 from api.tests.fixtures import mlsb_app, client, runner, sponsor_factory,\
     player_factory, league_event_factory, league_event_date_factory,\
     team_factory, league_factory, division_factory, bat_factory, game_factory,\
-    sample_league, espys_factory, auth, join_league_request_factory
+    espys_factory, auth, join_league_request_factory
