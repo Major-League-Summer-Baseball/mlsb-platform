5388f17a50a8865fcbd48922e95598d8ed384272
fras2560 <dallas.fraser.waterloo@gmail.com>
2024-02-02 08:51:05 -0500

    Add rest API for the team and some pytests

--- api/restx/__init__.py
+++ api/restx/__init__.py
@@ -2,6 +2,7 @@ from .fun import fun_api
 from .player import player_api
 from .sponsor import sponsor_api
 from .espys import espys_api
+from .team import team_api
 from flask_restx import Api
 apiX = Api(
     version="0.1",
@@ -19,3 +20,4 @@ apiX.add_namespace(fun_api)
 apiX.add_namespace(player_api)
 apiX.add_namespace(sponsor_api)
 apiX.add_namespace(espys_api)
+apiX.add_namespace(team_api)
