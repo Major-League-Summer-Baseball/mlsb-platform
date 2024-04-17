045e1171ce6d8f90c95da3d6f3224eab2c48fa3e
fras2560 <dallas.fraser.waterloo@gmail.com>
2024-04-16 22:05:52 -0400

    implement sponsor UI
    Able to update sponsor, create sponsor and hide/show sponsors

--- api/convenor/sponsors.py
+++ api/convenor/sponsors.py
@@ -1,15 +1,68 @@
-from flask import render_template, send_from_directory
+from flask import\
+    render_template, request, flash, session, redirect, url_for
 from api.variables import NOTFOUND, PICTURES, POSTS
 from api.cached_items import get_upcoming_games
-from api.authentication import get_user_information
-from api.convenor import convenor_blueprint
+from api.authentication import get_user_information, require_to_be_convenor
+from api.convenor import\
+    convenor_blueprint, normalize_model, is_empty, normalize_field
 from api.model import Sponsor
+from api.extensions import DB
 import os.path
 import json
 
 
 @convenor_blueprint.route("sponsors")
+@require_to_be_convenor
 def sponsors_page():
+    """All the sponsor pages"""
+    sponsors = [
+        sponsor.json()
+        for sponsor in Sponsor.query.order_by('sponsor_name').all()
+    ]
     return render_template(
-        "convenor/sponsors.html"
+        "convenor/sponsors.html",
+        sponsors=normalize_model(sponsors)
     )
+
+@convenor_blueprint.route("sponsors/submit", methods=["POST"])
+@require_to_be_convenor
+def submit_sponsor():
+    """Submit new player or changes to a player"""
+    sponsor_name = request.form.get("sponsor_name")
+    link = normalize_field(request.form.get("link", None))
+    description = normalize_field(request.form.get("description", None))
+    sponsor_id = request.form.get("sponsor_id", None)
+    try:
+        if is_empty(sponsor_id):
+            sponsor = Sponsor(sponsor_name, link=link, description=description)
+            DB.session.add(sponsor)
+            flash("Sponsor created")
+        else:
+            sponsor = Sponsor.query.get(sponsor_id)
+            if sponsor is None:
+                session['error'] = f"Sponsor does not exist {sponsor_id}" 
+                return redirect(url_for('convenor.error_page'))
+            sponsor.update(
+                name=sponsor_name, link=link, description=description
+            )
+            flash("sponsor updated")
+    except Exception as e:
+        session['error'] = str(e) 
+        return redirect(url_for('convenor.error_page'))
+    DB.session.commit()
+    return redirect(url_for("convenor.sponsors_page"))
+
+
+@convenor_blueprint.route(
+    "sponsors/<int:sponsor_id>/active/<int:visible>", methods=["POST"]
+)
+def change_visibility(sponsor_id: int, visible: int):
+    """Change the visibility of a sponsor."""
+    active = True if visible > 0 else False
+    sponsor = Sponsor.query.get(sponsor_id)
+    if sponsor is None:
+        session['error'] = f"Sponsor does not exist {sponsor_id}" 
+        return redirect(url_for('convenor.error_page'))
+    sponsor.update(active=active)
+    DB.session.commit()
+    return redirect(url_for("convenor.sponsors_page"))
