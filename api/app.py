d49d2f95019eebf4b45375023f345d5d2a227c51
fras2560 <dallas.fraser.waterloo@gmail.com>
2024-04-17 21:59:34 -0400

    Add ckeditor
    For editing html

--- api/app.py
+++ api/app.py
@@ -6,7 +6,7 @@ from werkzeug.middleware.proxy_fix import ProxyFix
 from os.path import join
 from api.variables import PICTURES, CSS_FOLDER, POSTS, FILES
 from api.routes import Routes
-from api.extensions import api, cache, DB, login_manager, tailsman
+from api.extensions import api, cache, DB, login_manager, tailsman, ckeditor
 from api.authentication import github_blueprint, facebook_blueprint,\
     google_blueprint, azure_blueprint, login_manager
 from api.documentation import documentation_blueprint
@@ -41,6 +41,8 @@ def register_extensions(app):
     cache.init_app(app)
     DB.init_app(app)
     login_manager.init_app(app)
+    ckeditor.init_app(app)
+    app.config['CKEDITOR_PKG_TYPE'] = 'full'
     if app.config["ENV"] != "development":
         csp = {
             'default-src': [
