a48151a98d4edcba6349cdc4e94962ff5c2f62d2
fras2560 <dallas.fraser.waterloo@gmail.com>
2024-04-24 08:31:17 -0400

    clear inputs before typing

--- ui-testing/cypress/features/convenor/fun/fun.ts
+++ ui-testing/cypress/features/convenor/fun/fun.ts
@@ -19,8 +19,8 @@ When(`I submit fun`, submitFun);
 const updateFunDetails = () => {
     const updatedFun = generateFun();
     cy.get<Fun>('@fun').then((fun: Fun) => {
-        cy.get(`#year${fun.fun_id}`).type(`${updatedFun.year}`);
-        cy.get(`#count${fun.fun_id}`).type(`${updatedFun.count}`);
+        cy.get(`#year${fun.fun_id}`).clear().type(`${updatedFun.year}`);
+        cy.get(`#count${fun.fun_id}`).clear().type(`${updatedFun.count}`);
         cy.get(`#funSubmit${fun.fun_id}`).click();
     });
 };
